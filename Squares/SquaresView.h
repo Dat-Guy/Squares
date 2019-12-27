ea40bd892072a0dde90492fed89ba3c1c9409816
Dat-Guy <samurott2004@gmail.com>
2019-12-27 07:09:20 -0500

    Initial Commit

---
+++ Squares/SquaresView.h
@@ -0,0 +1,13 @@
+//
+//  SquaresView.h
+//  Squares
+//
+//  Created by Aidan Smith on 12/27/19.
+//  Copyright © 2019 Aidan Smith. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface SquaresView : ScreenSaverView
+
+@end
---
+++ Squares/SquaresView.m
@@ -0,0 +1,52 @@
+//
+//  SquaresView.m
+//  Squares
+//
+//  Created by Aidan Smith on 12/27/19.
+//  Copyright © 2019 Aidan Smith. All rights reserved.
+//
+
+#import "SquaresView.h"
+
+@implementation SquaresView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
